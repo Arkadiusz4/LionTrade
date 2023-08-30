30cf76f95fc66b32a235da29c45f9c686bb87fc5
Arkadiusz4 <arekmika4@gmail.com>
2023-08-30 16:30:35 +0200

    Add colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LtColor {
+  const LtColor._();
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color navy = Color(0xFF003459);
+
+  static const Color black = Color(0xFF00171F);
+}
