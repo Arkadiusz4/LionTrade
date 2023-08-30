6912a9c5767a3b829990d2d14fb1bdeab5d7a9f0
Arkadiusz4 <arekmika4@gmail.com>
2023-08-30 23:17:16 +0200

    Add elevated button with svg

--- lib/widgets/buttons/lt_elevated_button.dart
+++ lib/widgets/buttons/lt_elevated_button.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/material.dart' hide Colors;
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lion_trade/theme/colors.dart';
 
 class LtElevatedButton extends StatelessWidget {
   final String text;
   final Function? onPressed;
+  final String? svgIconAsset;
+  final IconData? icon;
   final Color color;
   final Color colorText;
   final double minWidth;
@@ -11,11 +14,49 @@ class LtElevatedButton extends StatelessWidget {
   const LtElevatedButton({
     required this.text,
     required this.onPressed,
+    this.svgIconAsset,
+    this.icon,
     this.color = LtColor.navy,
     this.colorText = LtColor.white,
     this.minWidth = 150.0,
     Key? key,
   }) : super(key: key);
+
+  factory LtElevatedButton.withSvgIcon({
+    required String text,
+    required Function? onPressed,
+    required String svgIconAsset,
+    Color color = LtColor.navy,
+    Color colorText = LtColor.white,
+    double minWidth = 150.0,
+  }) {
+    return LtElevatedButton(
+      text: text,
+      onPressed: onPressed,
+      svgIconAsset: svgIconAsset,
+      color: color,
+      colorText: colorText,
+      minWidth: minWidth,
+    );
+  }
+
+  factory LtElevatedButton.withIcon({
+    required String text,
+    required Function? onPressed,
+    required IconData icon,
+    Color color = LtColor.navy,
+    Color colorText = LtColor.white,
+    double minWidth = 150.0,
+  }) {
+    return LtElevatedButton(
+      text: text,
+      onPressed: onPressed,
+      icon: icon,
+      color: color,
+      colorText: colorText,
+      minWidth: minWidth,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,12 +81,27 @@ class LtElevatedButton extends StatelessWidget {
             ),
           ),
         ),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 22.0,
-            color: colorText,
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null) Icon(icon),
+            if (svgIconAsset != null)
+              Padding(
+                padding: const EdgeInsets.only(right: 15.0),
+                child: SvgPicture.asset(
+                  svgIconAsset!,
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 22.0,
+                color: colorText,
+              ),
+            ),
+          ],
         ),
       ),
     );
