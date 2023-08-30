d5bcf104f9c26a70d288c28e8d560e5bdfff13f1
Arkadiusz4 <arekmika4@gmail.com>
2023-08-30 09:28:59 +0200

    Create custom buttons

--- lib/widgets/buttons/lt_elevated_button.dart
+++ lib/widgets/buttons/lt_elevated_button.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class LtButton extends StatelessWidget {
-  const LtButton({super.key});
+class LtElevatedButton extends StatelessWidget {
+  final String text;
+  final Function? onPressed;
+
+  const LtElevatedButton({
+    required this.text,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+        vertical: 15.0,
+      ),
+      child: ElevatedButton(
+        onPressed: () {
+          if (onPressed != null) {
+            onPressed;
+          }
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+          minimumSize: MaterialStateProperty.all(
+            const Size.fromHeight(50),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 22.0),
+        ),
+      ),
+    );
   }
 }
