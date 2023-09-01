fc698b4e4758477d731051f29858c001b2c3d85b
Arkadiusz4 <arekmika4@gmail.com>
2023-09-02 00:31:14 +0200

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lion_trade/screens/auth/auth_body.dart';
 import 'package:lion_trade/generated/l10n.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
