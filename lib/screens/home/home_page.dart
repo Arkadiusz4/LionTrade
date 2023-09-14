c7100c847f7424ad9de5c8693d10e4229268f299
Arkadiusz4 <arekmika4@gmail.com>
2023-09-14 11:37:11 +0200

    Add curved bottom navigation bar

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,11 +1,22 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lion_trade/screens/news/news_page.dart';
+import 'package:lion_trade/screens/portfolio/portfolio_page.dart';
+import 'package:lion_trade/screens/settings/settings_page.dart';
+import 'package:lion_trade/screens/stock/stock_page.dart';
+import 'package:lion_trade/theme/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   static Page page() => const MaterialPage<void>(child: HomePage());
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
@@ -13,6 +24,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(user!.email!),
             ElevatedButton(
---
+++ lib/screens/stock/stock_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StockPage extends StatelessWidget {
+  const StockPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Stock '),
+    );
+  }
+}
