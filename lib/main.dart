19725a6723b7b32ca770e29e41553292d8f1a733
Arkadiusz4 <arekmika4@gmail.com>
2023-09-26 16:22:42 +0200

    Add fireabse cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lion_trade/generated/l10n.dart';
 import 'package:lion_trade/screens/main_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
